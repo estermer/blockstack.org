174711006c9b24bf366104cbac4d3411b595a1ae
Ryan Shea <ryan@shea.io>
2016-04-19 17:57:06 -0400

    integrate blockstack profiles library to replace profile functions

--- app/js/pages/ProfilePage.js
+++ app/js/pages/ProfilePage.js
@@ -8,7 +8,6 @@ import { Person }    from 'blockstack-profiles'
 
 import Header        from '../components/Header'
 import Footer        from '../components/Footer'
-import { getName, getAvatarUrl }   from '../utils'
 
 class ProfilePage extends Component {
   constructor(props) {
@@ -45,34 +44,33 @@ class ProfilePage extends Component {
   }
 
   render() {
-    const profile = this.state.profile
-    const fullName = profile ? getName(profile) : ''
+    const person = new Person(this.state.profile || {})
 
     return (
-      <DocumentTitle title={`Blockstack - ${fullName}'s Profile`}>
+      <DocumentTitle title={`Blockstack - ${person.name()}'s Profile`}>
         <div>
           <div className="container-fluid col-centered navbar-fixed-top bg-primary">
             <Header />
           </div>
           <section className="feature-action col-centered">
             <div className="container-fluid proid-wrap p-t-4">
-              { profile !== null && profile !== undefined ?
+              { person !== null && person !== undefined ?
               <div className="col-sm-9">
                 <div className="container">
                   <div className="profile-container col-sm-6 center-block">
                     <div className="profile-wrap">
                       <div className="idcard-block">
                         <div className="id-flex">
-                          <img className="img-idcard" src={getAvatarUrl(profile)} />
+                          <img className="img-idcard" src={person.avatarUrl()} />
                           <div className="overlay"></div>
                         </div>
                       </div>
                     </div>
                     <div className="col-sm-6">
                       <div className="idcard-wrap">
-                        <h1 className="idcard-name">{getName(profile)}</h1>
+                        <h1 className="idcard-name">{person.name()}</h1>
                         <div className="idcard-body inverse">
-                          {profile.description}
+                          {person.toJSON().description}
                         </div>
                       </div>
                     </div>
